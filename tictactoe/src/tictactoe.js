052d9bf3b01405612b43c12f75c855ad69dc2dec
Qiyue Chen <jackchen20048@gmail.com>
2024-02-17 02:20:34 -0500

    making my own js for tictactoe

---
+++ tictactoe/src/tictactoe.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useState, useEffect} from 'react';
+
+import Table from 'react-bootstrap/Table';
+
+function Square() {
+
+}
+
+function Board() {
+
+}
+
+function Game() {
+  return(
+    <>
+      <h1>TicTacToe</h1>
+
+    </>
+  );
+}
+
+function TicTacToe() {
+  return(
+    <Game />
+
+  );
+
+}
+
+export default TicTacToe;
