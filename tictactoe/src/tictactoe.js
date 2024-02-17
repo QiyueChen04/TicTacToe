2b4b5a1b0e3bb4cf3464ee77c32831e3534c688d
Qiyue Chen <jackchen20048@gmail.com>
2024-02-17 06:04:55 -0500

    Draw message added. Added comments.

--- tictactoe/src/tictactoe.js
+++ tictactoe/src/tictactoe.js
@@ -2,33 +2,41 @@ import './tictactoe.css';
 import React from 'react';
 import {useState} from 'react';
 
+// Box component to represent squares in the TicTacToe board
 function Box({val, onChangeSquare}) {
   return(
     <button onClick = {onChangeSquare} className='square'> {val} </button>
   );
 }
 
-function Message({xTurn, winner}) {
+// Message component to display the current state of the game(win, draw, X's turn or O's turn)
+function Message({xTurn, winner, emptyBoxes}) {
   if(winner != null) {
     return(
       <p> Winner: {winner}</p>
     );
   }
+  if(emptyBoxes == 0) {
+    return (
+      <p> Draw </p>
+    );
+  }
+  if(xTurn) {
+    return (<p> Turn: X</p>);
+  }
   else {
-    if(xTurn) {
-      return (<p> Turn: X</p>);
-    }
-    else {
-      return (<p> Turn: O</p>);
-    }
+    return (<p> Turn: O</p>);
   }
 }
 
+// Board components
 function Board() {
   const [xTurn, setXTurn] = useState(true);
-  const [boardState, setBoardState] = useState([]);
+  const [boardState, setBoardState] = useState([]); 
   const [winner, setWinner] = useState();
+  const [emptyBoxes, setEmptyBoxes] = useState(9); // The empty boxes to track when the game is a draw
 
+  // Checks if there's a winner, if so, return the winner and the message component will be updated immediately
   function checkWin() {
     const wins = [
       [0, 1, 2],
@@ -52,18 +60,20 @@ function Board() {
     }
   }
 
+  // When a box (child of the Board component) is clicked, if the game isn't over and box is empty
+  // then update the board accordingly
   function handleChangeSquare(index) {
     if(!winner && boardState.at(index) == null) {
         if(xTurn) {
           boardState[index] = 'X'; 
-          setBoardState(boardState);
           setXTurn(false);
         }
         else {
           boardState[index] = 'O';
-          setBoardState(boardState);
           setXTurn(true);
         }
+        setBoardState(boardState);
+        setEmptyBoxes(emptyBoxes - 1);
         checkWin();
     }
   }
@@ -71,20 +81,18 @@ function Board() {
   return(
     <>
       <h1>TicTacToe</h1>
-      <Message xTurn={xTurn} winner={winner}/>
+      <Message xTurn={xTurn} winner={winner} emptyBoxes={emptyBoxes}/>
       <table align="center">
         <tr>
           <Box val = {boardState[0]} onChangeSquare={() => handleChangeSquare(0)}/>
           <Box val = {boardState[1]} onChangeSquare={() => handleChangeSquare(1)}/>
           <Box val = {boardState[2]} onChangeSquare={() => handleChangeSquare(2)}/>
         </tr>
-          
         <tr>
           <Box val = {boardState[3]} onChangeSquare={() => handleChangeSquare(3)}/>
           <Box val = {boardState[4]} onChangeSquare={() => handleChangeSquare(4)}/>
           <Box val = {boardState[5]} onChangeSquare={() => handleChangeSquare(5)}/>
         </tr>
-
         <tr>
           <Box val = {boardState[6]} onChangeSquare={() => handleChangeSquare(6)}/>
           <Box val = {boardState[7]} onChangeSquare={() => handleChangeSquare(7)}/>
